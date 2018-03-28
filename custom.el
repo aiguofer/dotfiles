69b9d0bce8dc7951903966ced5c9191c7cc38480
Diego Fernandez <difernan@redhat.com>
2018-03-27 23:29:11 -0600

    Add safe themes

--- custom.el
+++ custom.el
@@ -25,7 +25,7 @@
     (company-sort-by-backend-importance company-sort-by-occurrence company-sort-by-statistics)))
  '(custom-safe-themes
    (quote
-    ("3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa" default)))
+    ("3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa" "a27c00821ccfd5a78b01e4f35dc056706dd9ede09a8b90c6955ae6a390eb1c1e" default)))
  '(ecb-options-version "2.40")
  '(ecb-source-path (quote (("/" "/"))))
  '(ecb-tip-of-the-day nil)
