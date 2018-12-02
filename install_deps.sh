52c7216bf886be61755a1d61e43d06828fce5fb4
Diego Fernandez <aiguo.fernandez@gmail.com>
2018-12-02 19:12:37 -0300

    Improve handling of python environment

--- install_deps.sh
+++ install_deps.sh
@@ -12,12 +12,26 @@ for installer in $git_installers; do
     curl -sL https://raw.githubusercontent.com/$installer | bash
 done
 
+# setup python environment
+pyenv virtualenv --system-site-packages -p /usr/bin/python2 default2
+pyenv virtualenv --system-site-packages -p /usr/bin/python3 default3
+
+pyenv global default3 default2
+
+pip2 install -r requirements.txt
+pip3 install -r requirements.txt
+
 # instal pipx and python executables
 curl -sL https://raw.githubusercontent.com/cs01/pipx/master/get-pipx.py | PYENV_VERSION=system python3
 
 for pkg in $(cat ./requirements_pipx.txt);do
     pipx install $pkg
 done
+
+# enable emacs keys everywhere
+emacs-keys-everywhere
+# setup python environment
+
 
 # get zsh completions
 ./generate_completions.sh
