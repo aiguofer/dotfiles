7a47c07fea509d6137a9f445d56aea4abc9380b5
Diego Fernandez <aiguo.fernandez@gmail.com>
2019-07-01 19:00:28 -0600

    Clean unneeded custom settings

--- custom.el
+++ custom.el
@@ -28,17 +28,6 @@
     (company-sort-by-backend-importance company-sort-by-occurrence company-sort-by-statistics)))
  '(confirm-kill-processes nil)
  '(create-lockfiles nil)
- '(custom-safe-themes
-   (quote
-    ("3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa" "a27c00821ccfd5a78b01e4f35dc056706dd9ede09a8b90c6955ae6a390eb1c1e" default)))
- '(ecb-options-version "2.40")
- '(ecb-source-path (quote (("/" "/"))))
- '(ecb-tip-of-the-day nil)
- '(ecb-tree-indent 2)
- '(ecb-windows-width 0.2)
- '(ein:use-auto-complete t)
- '(ein:use-auto-complete-superpack t)
- '(elpy-mode-hook (quote (subword-mode)))
  '(elpy-modules
    (quote
     (elpy-module-company elpy-module-eldoc elpy-module-highlight-indentation elpy-module-yasnippet elpy-module-django elpy-module-autodoc elpy-module-sane-defaults)))
@@ -61,7 +50,6 @@
  '(magit-pull-arguments (quote ("--rebase")))
  '(markdown-command "/usr/bin/pandoc")
  '(mode-require-final-newline (quote visit-save))
- '(powerline-default-separator (quote utf-8))
  '(powerline-gui-use-vcs-glyph t)
  '(projectile-completion-system (quote helm))
  '(projectile-mode-line nil)
@@ -69,7 +57,6 @@
  '(python-shell-completion-native-disabled-interpreters (quote ("pypy" "ipython" "jupyter")))
  '(python-shell-prompt-detect-failure-warning nil)
  '(require-final-newline (quote visit-save))
- '(rm-blacklist (quote (" Wrap" " Helm" " hl-p")))
  '(semantic-default-submodes
    (quote
     (global-semantic-idle-scheduler-mode global-semanticdb-minor-mode global-semantic-idle-local-symbol-highlight-mode)))
