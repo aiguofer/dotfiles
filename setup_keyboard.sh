48ce99f4b05f1865168e6382d70e84d6bd62a072
Diego Fernandez <aiguo.fernandez@gmail.com>
2019-07-31 01:50:35 -0600

    Remove unneeded function call

--- setup_keyboard.sh
+++ setup_keyboard.sh
@@ -3,9 +3,6 @@
 # enable emacs keys everywhere
 emacs-keys-everywhere
 
-# get zsh completions
-./generate_completions.sh
-
 # set up Caps as extra Ctrl key
 localectl set-x11-keymap us pc105 "" ctrl:nocaps
 
