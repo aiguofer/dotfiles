ad9c9144dea0899c86ff7f252012d810180dfc82
Diego Fernandez <aiguo.fernandez@gmail.com>
2020-01-20 16:03:29 -0700

    Fix file symlinking and only do system sym links for linux
    If file paths have spaces, splitting the output of find into an array
    will break on those spaces. This change ensures each path is treated individually.

--- setup_symlinks.sh
+++ setup_symlinks.sh
@@ -15,35 +15,45 @@ user_dirs=(
     user/.emacs.d
 )
 
+
 for dir in "${user_dirs[@]}"; do
     dest_dir=${dir/"user"/$HOME}
     # skip if dir is already symlinked
     if [[ -L "$dest_dir" && -d "$dest_dir" ]]; then continue; fi
+
     parent_dir=$(echo $dir | sed -E 's/\/[^/]*$//')
     mkdir -p $parent_dir
     ln -sf $(pwd)/$dir $dest_dir
 done
 
 echo "Symlinking user files"
-user_files=$(find user \( -type f -o -type l \))
+user_files=()
+while IFS=  read -r -d $'\0' file; do
+    user_files+=("$file")
+done < <(find user \( -type f -o -type l  \) -print0)
 
-for file in $user_files; do
-    # skip files in the symlinked directories
+
+for file in "${user_files[@]}"; do
+    # # skip files in the symlinked directories
     if array_contains $file "${user_dirs[@]}"; then continue; fi
+
     dest_file=${file/"user"/$HOME}
     dir=$(echo $dest_file | sed -E 's/\/[^/]*$//')
+
     mkdir -p $dir
     ln -sf $(pwd)/$file $dest_file
 done
 
 echo
 
-echo "Symlinking system files"
-system_files=$(find system \( -type f -o -type l \))
+if [[ "$OSTYPE" == linux* ]]; then
+    echo "Symlinking system files"
+    system_files=$(find system \( -type f -o -type l \))
 
-for file in $system_files; do
-    dest_file=${file/"system"/}
-    dir=$(echo $dest_file | sed -E 's/\/[^/]*$//')
-    sudo mkdir -p $dir
-    sudo ln -sf $(pwd)/$file $dest_file
-done
+    for file in $system_files; do
+        dest_file=${file/"system"/}
+        dir=$(echo $dest_file | sed -E 's/\/[^/]*$//')
+        sudo mkdir -p $dir
+        sudo ln -sf $(pwd)/$file $dest_file
+    done
+fi
