9e3a290277e0ce89d602ed99de1cc195970ba1c4
Diego Fernandez <aiguo.fernandez@gmail.com>
2020-03-17 12:59:13 -0600

    Add comment to emacs file

--- user/.emacs.d/init.el
+++ user/.emacs.d/init.el
@@ -307,6 +307,7 @@
     (elpy-enable)
     :config
     (setq elpy-modules (delq 'elpy-module-flymake elpy-modules))
+    ; fix for MacOS, see https://github.com/jorgenschaefer/elpy/issues/1550
     (setq elpy-shell-echo-output nil)
     (setq elpy-rpc-python-command "python3")
     (setq elpy-rpc-timeout 2))
