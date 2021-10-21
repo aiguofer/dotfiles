5995d4a181e68376d6962d02b26a326d27e342bc
Diego Fernandez <aiguo.fernandez@gmail.com>
2021-10-20 22:06:37 -0600

    Don't create python2 default pyenv env

--- setup_python.sh
+++ setup_python.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
 # setup python environment
-pyenv virtualenv --system-site-packages -p /usr/bin/python2 default2
 pyenv virtualenv --system-site-packages -p /usr/bin/python3 default3
 
-pyenv global default3 default2
+pyenv global default3
 
-pip2 install -r packages/python_global.txt
 pip3 install -r packages/python_global.txt
