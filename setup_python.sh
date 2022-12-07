5baf0832f490724a2cda65bc2085bad51af868e6
Diego Fernandez <aiguo.fernandez@gmail.com>
2022-12-07 10:22:30 -0700

    remove python2

--- setup_python.sh
+++ setup_python.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # setup python environment
-pyenv virtualenv --system-site-packages -p /usr/bin/python3 default3
+pyenv virtualenv --system-site-packages -p /usr/bin/python3 default
 
-pyenv global default3
+pyenv global default
 
 pip3 install -r packages/python_global.txt
