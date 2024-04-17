2ffe3191aaffaa5ae80ffde7d84ef043d011eb52
Diego Fernandez <aiguo.fernandez@gmail.com>
2024-04-16 19:16:11 -0700

    Fix sketchybar wifi

--- user/.config/sketchybar/plugins/wifi.sh
+++ user/.config/sketchybar/plugins/wifi.sh
@@ -2,7 +2,7 @@
 
 update() {
   source "$CONFIG_DIR/icons.sh"
-  INFO="$(/System/Library/PrivateFrameworks/Apple80211.framework/Resources/airport -I | awk -F ' SSID: '  '/ SSID: / {print $2}')"
+  INFO="$(networksetup -listallhardwareports | awk '/Wi-Fi/{getline; print $2}' | xargs networksetup -getairportnetwork | sed "s/Current Wi-Fi Network: //")"
   LABEL="$INFO ($(ipconfig getifaddr en0))"
   ICON="$([ -n "$INFO" ] && echo "$WIFI_CONNECTED" || echo "$WIFI_DISCONNECTED")"
 
