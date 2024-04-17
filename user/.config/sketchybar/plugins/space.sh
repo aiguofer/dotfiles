c43fd474bb926463bbab0aad143831079e2f67e9
Diego Fernandez <aiguo.fernandez@gmail.com>
2024-04-16 17:46:18 -0700

    Yabai improvements and add sketchybar

---
+++ user/.config/sketchybar/plugins/space.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+update() {
+  source "$CONFIG_DIR/colors.sh"
+  COLOR=$BACKGROUND_2
+  if [ "$SELECTED" = "true" ]; then
+    COLOR=$GREY
+  fi
+  sketchybar --set $NAME icon.highlight=$SELECTED \
+                         label.highlight=$SELECTED \
+                         background.border_color=$COLOR
+}
+
+mouse_clicked() {
+  if [ "$BUTTON" = "left" ]; then
+    yabai -m space --focus $SID 2>/dev/null
+  fi
+}
+
+case "$SENDER" in
+  "mouse.clicked") mouse_clicked
+  ;;
+  *) update
+  ;;
+esac
